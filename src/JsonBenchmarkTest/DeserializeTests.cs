70ba23324f35509c6bb459008465705a248c1bae
Fawel <uiasop@gmail.com>
2020-06-13 15:44:23 +0300

    Добавил бенчмврки с десериализацией из строки с Json

--- src/JsonBenchmarkTest/DeserializeTests.cs
+++ src/JsonBenchmarkTest/DeserializeTests.cs
@@ -13,7 +13,7 @@ namespace JsonBenchmarkTest
     public class DeserializeTests
     {
         private readonly string _pathToJsonFile =
-            "C:\\Users\\Fawel\\Desktop\\Progs\\JsonBencmarkTests\\src\\TestObjects10k.json";
+            "C:\\Users\\Fawel\\Desktop\\Progs\\JsonBencmarkTests\\src\\TestObjects1k.json";
 
         [Benchmark]
         public Human[] Use_NewtonsoftJson()
@@ -38,5 +38,30 @@ namespace JsonBenchmarkTest
             using var jsonStream = new StreamReader(_pathToJsonFile);
             return Utf8Json.JsonSerializer.Deserialize<Human[]>(jsonStream.BaseStream);
         }
+
+        [Benchmark]
+        public Human[] Use_NewtonsoftJson_FromString()
+        {
+            using var jsonStream = new StreamReader(_pathToJsonFile);
+            var jsonString = jsonStream.ReadToEnd();
+            return Newtonsoft.Json.JsonConvert.DeserializeObject<Human[]>(jsonString);
+        }
+
+        [Benchmark]
+        public Human[] Use_Jil_FromString()
+        {
+            using var jsonStream = new StreamReader(_pathToJsonFile);
+            var jsonString = jsonStream.ReadToEnd();
+            return Jil.JSON.Deserialize<Human[]>(jsonString);
+        }
+
+        [Benchmark]
+        public Human[] Use_Utf8Json_FromString()
+        {
+            using var jsonStream = new StreamReader(_pathToJsonFile);
+            var jsonString = jsonStream.ReadToEnd();
+            return Utf8Json.JsonSerializer.Deserialize<Human[]>(jsonString);
+        }
+
     }
 }
